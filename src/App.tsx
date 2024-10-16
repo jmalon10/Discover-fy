e6405121ddef670ce567258adce58c10c617c73b
Jackie Maloney <jmaloney11277@gmail.com>
2024-10-16 15:52:07 -0400

    deletes VITE default counter that wasnt being used that was interfearing with the render deployment

--- src/App.tsx
+++ src/App.tsx
@@ -11,9 +11,7 @@ function App() {
      
       <h1>hello world</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
         
-        </button>
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
