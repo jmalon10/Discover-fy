07d3e6082ea3d0072635790b217024b1383d66b2
Ryan Merlini <rdmerlini@gmail.com>
2024-10-18 11:49:16 -0400

    adding useEffect Hook and useState for RecommendedArtist object

--- client/src/pages/Discover.tsx
+++ client/src/pages/Discover.tsx
@@ -1,7 +1,23 @@
+import { useEffect, useState } from "react";
+
 const Discover = () => {
+    const [artist, setArtist] = useState<RecommendedArtist | null>({} as RecommendedArtist);
+    // when the component loads...
+    useEffect(() => {
+      getRecommendedArtistData();
+    }, []);
+    const getRecommendedArtistData = async () => {
+      // fetch the data from the API
+      const response = await fetch("/api/recommendedArtists");
+      const data = await response.json();
+      console.log(data);
+    }
+    //  we want to fetch the artist data and put it in state
+
     return (
       <section>
         <h1>This is the discover page!</h1>
+
         </section>
     );
   };
