2cb155ea76f5a77f7aeb6bb86ebc678ba8b31a26
lekshmisree@gmail.com <lekshmisree89@gmail.com>
2024-10-18 17:21:40 -0400

    home.tsx modified for imports

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -4,50 +4,8 @@
 import { Link } from 'react-router-dom';
 import '../index.css';   
 
-import { useState, useEffect, useLayoutEffect } from "react";
-import { retrieveUsers } from "../api/userAPI";
-import type { UserData } from "../interfaces/UserData";
-import ErrorPage from "./ErrorPage";
-import auth from '../utils/auth';
 
 const HomePage = () => {
-
-    const [users, setUsers] = useState<UserData[]>([]);
-    const [error, setError] = useState(false);
-    const [loginCheck, setLoginCheck] = useState(false);
-
-    useEffect(() => {
-        if (loginCheck) {
-            fetchUsers();
-        }
-    }, [loginCheck]);
-
-    useLayoutEffect(() => {
-        checkLogin();
-    }, []);
-
-    const checkLogin = () => {
-        if (auth.loggedIn()) {
-            setLoginCheck(true);
-        }
-    };
-
-    const fetchUsers = async () => {
-        try {
-            const data = await retrieveUsers();
-            setUsers(data)
-        } catch (err) {
-            console.error('Failed to retrieve tickets:', err);
-            setError(true);
-        }
-    }
-
-    if (error) {
-        return <ErrorPage />;
-    }
-
-
-
 
   return (
 
