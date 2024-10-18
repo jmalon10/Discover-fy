a4e4e303401dfaa8d853ac5e6f5a507c8d259062
Jackie Maloney <jmaloney11277@gmail.com>
2024-10-18 00:42:41 -0400

    adds a playlist page and corresponding route

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -8,6 +8,7 @@ import ErrorPage from './pages/ErrorPage.tsx';
 import Home from './pages/Home.tsx';
 import Login from './pages/Login.tsx';
 import Discover from './pages/Discover.tsx';
+import Playlists from './pages/Playlists.tsx';
 
 const router = createBrowserRouter([
   {
@@ -26,6 +27,10 @@ const router = createBrowserRouter([
       {
         path: '/discover',
         element: <Discover />
+      }, 
+      {
+        path: '/playlists',
+        element: <Playlists />
       }, 
     ]
   }
---
+++ client/src/pages/Playlists.tsx
@@ -0,0 +1,9 @@
+const Playlists = () => {
+    return (
+      <section>
+        <h1>This is the page you will see your newly generated playlist!</h1>
+        </section>
+    );
+  };
+  
+  export default Playlists;
