6ba9af360b5a7234513c6ef4cfca71bb7eb19e6b
Ryan Merlini <rdmerlini@gmail.com>
2024-10-18 11:17:21 -0400

    added imports for RecommendedArtist component and defined props for recommended artist card

---
+++ client/src/components/RecommendedArtistCard.tsx
@@ -0,0 +1,8 @@
+import type React from 'react';
+import type { RecommendedArtist } from '../interfaces/recommendedArtist';
+
+// Define the props for the component
+interface RecommendedArtistCardProps {
+    artist: RecommendedArtist; // artist is a RecommendedArtist object
+}
+
