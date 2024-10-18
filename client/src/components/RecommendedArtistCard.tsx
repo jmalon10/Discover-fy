f88ab1d93cb07a801b90b54a8aa370f5467622a6
Ryan Merlini <rdmerlini@gmail.com>
2024-10-18 11:18:50 -0400

    defined component to include carde title,and mapping through list items of top tracks

--- client/src/components/RecommendedArtistCard.tsx
+++ client/src/components/RecommendedArtistCard.tsx
@@ -6,3 +6,20 @@ interface RecommendedArtistCardProps {
     artist: RecommendedArtist; // artist is a RecommendedArtist object
 }
 
+// Define the component
+const RecommendedArtistCard: React.FC<RecommendedArtistCardProps> = ({ artist }) => {
+    return (
+        <div className="card">
+            <div className="card-body">
+                <h5 className="card-title">{artist.Name}</h5>
+                <ul>
+                    {artist.TopTracks.map((track, index) => (
+                        <li key={index}>{track}</li>
+                    ))}
+                </ul>
+            </div>
+        </div>
+    );
+};
+
+export default RecommendedArtistCard;
