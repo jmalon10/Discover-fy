bfaf68616ba4017199c8f88e293d14d3bf81dd37
Kol Wangchuk <kolwangchuk@kols-air-2.mynetworksettings.com>
2024-10-18 13:27:58 -0400

    playlist inteface

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -2,7 +2,7 @@ import { useState, useEffect, useLayoutEffect } from "react";
 import { retrieveUsers } from "../api/userAPI";
 import type { UserData } from "../interfaces/UserData";
 import ErrorPage from "./ErrorPage";
-import UserList from '../components/Playlists';
+import UserList from '../components/Playlist';
 import auth from '../utils/auth';
 
 const Home = () => {
