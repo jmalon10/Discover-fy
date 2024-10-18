14cd41355367c3b6b7a39b2e7f0b31f454634669
Ryan Merlini <rdmerlini@gmail.com>
2024-10-18 14:53:40 -0400

    creating a retrieveArtists function

--- client/src/api/artistsAPI.tsx
+++ client/src/api/artistsAPI.tsx
@@ -1,2 +1,17 @@
 import Auth from '../utils/auth';
 
+const retrieveArtists = async () => {
+    try {
+      const response = await fetch('/api/artists', {
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: `Bearer ${Auth.getToken()}`
+        }
+      });
+      const data = await response.json();
+  
+
+  }
+  
+  export { retrieveArtists };
+  
