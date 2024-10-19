83a96ffedb9ba98d8b178af2ba92127c6ebe9a57
Judymae Jolibois <judymaejolibois12388@gmail.com>
2024-10-19 11:56:48 -0400

    added basic code to  tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,11 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: [],
+export default {
+  content: [
+    "./index.html", // Include your main HTML file.
+    "./src/**/*.{js,ts,jsx,tsx}", // Include all JS, TS, JSX, and TSX files in the src directory.
+  ],
   theme: {
-    extend: {},
+    extend: {}, // This is where you can customize your theme, such as adding new colors or fonts.
   },
-  plugins: [],
-}
-
+  plugins: [], // Add Tailwind plugins here if you need extra functionality.
+};
