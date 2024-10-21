b2fbd84dffdb3d526a6a27eb6d108738df5a8060
kol <kol3wang@gmail.com>
2024-10-21 19:48:28 -0400

    deleted Create the users table in db

--- server/db/schema.sql
+++ server/db/schema.sql
@@ -7,12 +7,5 @@ CREATE DATABASE users_db;
 -- Switch to the new database
 \c users_db
 
--- Create the users table
-CREATE TABLE users (
-    id SERIAL PRIMARY KEY,
-    username VARCHAR(255) NOT NULL UNIQUE,
-    email VARCHAR(255) NOT NULL UNIQUE,
-    password VARCHAR(255) NOT NULL,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-);
+
 
