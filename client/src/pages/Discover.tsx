70b2c91cfab267d274a5a1b18ee467fb8042e25d
Jiju <178104698+jiju-codes@users.noreply.github.com>
2024-10-21 16:02:03 -0400

    Add margin-left to input box

--- client/src/pages/Discover.tsx
+++ client/src/pages/Discover.tsx
@@ -67,7 +67,7 @@ const Discover = () => {
         <label htmlFor="favoriteArtist">Enter your favorite artist:</label>
         <input
           type="text"
-          className="text-black"
+          className="text-black ml-2"
           id="favoriteArtist"
           value={favoriteArtist}
           onChange={(e) => setFavoriteArtist(e.target.value)} // Update state when user types
