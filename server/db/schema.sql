1f4586576d5301c6acca604a2a28c90843f621c9
Jackie Maloney <jmaloney11277@gmail.com>
2024-10-21 18:25:03 -0400

    delete unnecessary tables in the DB already being made by sequalize

--- server/db/schema.sql
+++ server/db/schema.sql
@@ -3,25 +3,3 @@ DROP DATABASE IF EXISTS users_db;
 
 -- CREATE DATABASE
 CREATE DATABASE users_db;
-
--- Switch to the new database
-\c users_db
-
--- Create the users table
-CREATE TABLE users (
-    id SERIAL PRIMARY KEY,
-    username VARCHAR(255) NOT NULL UNIQUE,
-    email VARCHAR(255) NOT NULL UNIQUE,
-    password VARCHAR(255) NOT NULL,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-);
-
--- Create the playlists table
-CREATE TABLE playlists (
-    id SERIAL PRIMARY KEY,
-    user_id INT REFERENCES users(id) ON DELETE CASCADE, -- Link each playlist to a user
-    title VARCHAR(255) NOT NULL,
-    image VARCHAR(255), -- Store the image URL for the playlist
-    tracks JSONB NOT NULL, -- Store the list of tracks as a JSON array
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-);
