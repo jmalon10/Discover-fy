27af78384d24d36821382dfd8d9e5a30c39ede31
kol <kol3wang@gmail.com>
2024-10-21 19:45:35 -0400

    deleted the create the playlist table in db

--- server/db/schema.sql
+++ server/db/schema.sql
@@ -16,12 +16,3 @@ CREATE TABLE users (
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
--- Create the playlists table
-CREATE TABLE playlists (
-    id SERIAL PRIMARY KEY,
-    user_id INT REFERENCES users(id) ON DELETE CASCADE, -- Link each playlist to a user
-    title VARCHAR(255) NOT NULL,
-    image VARCHAR(255), -- Store the image URL for the playlist
-    tracks JSONB NOT NULL, -- Store the list of tracks as a JSON array
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-);
